f76489e225c8dfb490297a5a6b9dd898967e5429
Zach <z.kirsanov@gmail.com>
2019-07-03 20:47:45 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var leftBorderWith = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[2]);
+
+alert("Hello World");
+/* let answer = confirm("Are you gay?");
+console.log(answer); */
+let answer = prompt("Есть", "ХЗ");
+console.log(answer);
